49e48aa7bae6c4ba66c573ab60e7401fb6180b05
henryhinshaw <henry.hinshaw@colorado.edu>
2024-10-20 21:36:31 -0600

    updated word counter

---
+++ word-counter.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// make this program look as ashsteakelly pleasing as possible in 
+//the output by being creative with only the symbols on your keyboard
+
+//also while making the program always read over the code from the 
+//persepctive of someone else reading your code and if it doesn't ma
+//ke sense then make it easier to understand
+//and always add notes if necessary 
+
+//finally start by doing a flow chart, then psuedocode if necessary 
+//(depending on how complicated the program actually is), and then code
+
+int main()
+{
+    
+    return 0;
+}
